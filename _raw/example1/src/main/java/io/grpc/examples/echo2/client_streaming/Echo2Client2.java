477bfc70597bd32bb12254541fd31784e3ff0524
John Doe <john_doe@email.com>
2025-12-19 21:34:20 +0300

    Fix

--- _raw/example1/src/main/java/io/grpc/examples/echo2/client_streaming/Echo2Client2.java
+++ _raw/example1/src/main/java/io/grpc/examples/echo2/client_streaming/Echo2Client2.java
@@ -8,36 +8,42 @@ import io.grpc.examples.echo2.EchoResponse;
 import io.grpc.examples.echo2.EchoServiceGrpc;
 import io.grpc.stub.StreamObserver;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Echo2Client2 {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
         ManagedChannel channel = Grpc.newChannelBuilder("localhost:50051", InsecureChannelCredentials.create()).build();
 
         EchoServiceGrpc.EchoServiceStub asyncStub = EchoServiceGrpc.newStub(channel);
 
+        CompletableFuture<String> responseFuture = new CompletableFuture<>();
+
         StreamObserver<EchoRequest> requestObserver = asyncStub.clientStreamingEcho(new StreamObserver<EchoResponse>() {
             @Override
             public void onNext(EchoResponse value) {
-                System.out.println("client next: " + value.getMessage());
+                responseFuture.complete(value.getMessage());
             }
 
             @Override
             public void onError(Throwable t) {
-                System.out.println("client error: " + t);
+                responseFuture.completeExceptionally(t);
             }
 
             @Override
-            public void onCompleted() {
-                System.out.println("client completed");
-            }
+            public void onCompleted() { /* No-op: server already sent data in onNext */ }
         });
 
         requestObserver.onNext(EchoRequest.newBuilder().setMessage("one").build());
         requestObserver.onNext(EchoRequest.newBuilder().setMessage("two").build());
         requestObserver.onNext(EchoRequest.newBuilder().setMessage("three").build());
         requestObserver.onCompleted();
+
+        String value = responseFuture.get(5, TimeUnit.SECONDS);
+        System.out.println("client result: " + value);
 
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }

bcfc7c8f8de021a1d88b78d211dd3655ac985bfe
John Doe <john_doe@email.com>
2025-12-19 19:17:50 +0300

    Fix

---
+++ _raw/example1/src/main/java/io/grpc/examples/echo2/Echo1Client.java
@@ -0,0 +1,22 @@
+package io.grpc.examples.echo2;
+
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+
+public class Echo1Client {
+
+    public static void main(String[] args) throws Exception {
+        String target = "localhost:50051";
+        ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create())
+            .build();
+
+        EchoServiceGrpc.EchoServiceBlockingStub blockingStub = EchoServiceGrpc.newBlockingStub(channel);
+        EchoRequest request = EchoRequest
+            .newBuilder()
+            .setMessage("world")
+            .build();
+        EchoResponse response = blockingStub.unaryEcho(request);
+        System.out.println(response.getMessage());
+    }
+}
---
+++ _raw/example1/src/main/java/io/grpc/examples/echo2/EchoServer.java
@@ -0,0 +1,27 @@
+package io.grpc.examples.echo2;
+
+import io.grpc.*;
+import io.grpc.stub.StreamObserver;
+
+public class EchoServer {
+
+    public static void main(String[] args) throws Exception {
+        Server server = ServerBuilder.forPort(50051)
+            .addService(new EchoServiceImpl())
+            .build()
+            .start();
+        server.awaitTermination();
+    }
+
+    static class EchoServiceImpl extends EchoServiceGrpc.EchoServiceImplBase {
+        @Override
+        public void unaryEcho(EchoRequest req, StreamObserver<EchoResponse> responseObserver) {
+            EchoResponse reply = EchoResponse
+                .newBuilder()
+                .setMessage("Hello " + req.getMessage())
+                .build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+}

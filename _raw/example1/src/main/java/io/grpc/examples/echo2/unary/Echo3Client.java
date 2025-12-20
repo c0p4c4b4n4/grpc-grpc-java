54a4faf8f733b18fc30c464acaacf1a6628783fc
John Doe <john_doe@email.com>
2025-12-20 20:49:56 +0300

    Fix

---
+++ _raw/example1/src/main/java/io/grpc/examples/echo2/unary/Echo3Client.java
@@ -0,0 +1,48 @@
+package io.grpc.examples.echo2.unary;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+import io.grpc.examples.echo2.EchoRequest;
+import io.grpc.examples.echo2.EchoResponse;
+import io.grpc.examples.echo2.EchoServiceGrpc;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class Echo3Client {
+
+    public static void main(String[] args) throws Exception {
+        String target = "localhost:50051";
+        ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create())
+            .build();
+
+        CountDownLatch finishLatch = new CountDownLatch(1);
+
+        EchoServiceGrpc.EchoServiceFutureStub futureStub = EchoServiceGrpc.newFutureStub(channel);
+        ListenableFuture<EchoResponse> responseFuture = futureStub.unaryEcho(EchoRequest.newBuilder().setMessage("world").build());
+        Futures.addCallback(responseFuture, new FutureCallback<EchoResponse>() {
+            @Override
+            public void onSuccess(EchoResponse result) {
+                System.out.println(result.getMessage());
+                finishLatch.countDown();
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                finishLatch.countDown();
+            }
+        }, MoreExecutors.directExecutor());
+
+        if (!finishLatch.await(1, TimeUnit.MINUTES)) {
+            System.err.println("Calls did not finish within timeout.");
+        }
+
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+}
+

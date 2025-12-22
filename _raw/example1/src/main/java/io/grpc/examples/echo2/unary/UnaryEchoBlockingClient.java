59aaeda158b5a293ddbd12feddbb4bf47c3dbc52
John Doe <john_doe@email.com>
2025-12-22 15:40:30 +0300

    Fix

---
+++ _raw/example1/src/main/java/io/grpc/examples/echo2/unary/UnaryEchoBlockingClient.java
@@ -0,0 +1,27 @@
+package io.grpc.examples.echo2.unary;
+
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+import io.grpc.examples.echo2.EchoRequest;
+import io.grpc.examples.echo2.EchoResponse;
+import io.grpc.examples.echo2.EchoServiceGrpc;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class UnaryEchoBlockingClient {
+
+    private static final Logger logger = Logger.getLogger(UnaryEchoBlockingClient.class.getName());
+
+    public static void main(String[] args) throws Exception {
+        ManagedChannel channel = Grpc.newChannelBuilder("localhost:50051", InsecureChannelCredentials.create()).build();
+
+        EchoServiceGrpc.EchoServiceBlockingStub blockingStub = EchoServiceGrpc.newBlockingStub(channel);
+        EchoRequest request = EchoRequest.newBuilder().setMessage("world").build();
+        EchoResponse response = blockingStub.unaryEcho(request);
+        System.out.println("result: " + response.getMessage());
+
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+}
---
+++ _raw/example1/src/main/java/io/grpc/examples/echo2/unary/UnaryEchoServer.java
@@ -0,0 +1,33 @@
+package io.grpc.examples.echo2.unary;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.examples.echo2.EchoRequest;
+import io.grpc.examples.echo2.EchoResponse;
+import io.grpc.examples.echo2.EchoServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+import java.util.logging.Logger;
+
+public class UnaryEchoServer {
+
+    private static final Logger logger = Logger.getLogger(UnaryEchoServer.class.getName());
+
+    public static void main(String[] args) throws Exception {
+        Server server = ServerBuilder.forPort(50051)
+            .addService(
+                new EchoServiceGrpc.EchoServiceImplBase() {
+                    @Override
+                    public void unaryEcho(EchoRequest request, StreamObserver<EchoResponse> responseObserver) {
+                        EchoResponse response = EchoResponse.newBuilder().setMessage("hello " + request.getMessage()).build();
+                        responseObserver.onNext(response);
+                        responseObserver.onCompleted();
+                    }
+                }
+            )
+            .build()
+            .start();
+
+        server.awaitTermination();
+    }
+}

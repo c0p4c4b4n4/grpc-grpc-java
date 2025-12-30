1b07c01f41215649c62bf959fc0fac6d236bd71a
John Doe <john_doe@email.com>
2025-12-30 16:50:56 +0300

    Fix

--- example1/src/main/java/com/example/grpc/feature/keepalive/UnaryServer.java
+++ example1/src/main/java/com/example/grpc/feature/keepalive/UnaryServer.java
@@ -1,11 +1,14 @@
 package com.example.grpc.feature.keepalive;
 
-import com.example.grpc.Servers;
+import com.example.grpc.Loggers;
 import com.example.grpc.echo.EchoRequest;
 import com.example.grpc.echo.EchoResponse;
 import com.example.grpc.echo.EchoServiceGrpc;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class UnaryServer {
@@ -13,7 +16,28 @@ public class UnaryServer {
     private static final Logger logger = Logger.getLogger(UnaryServer.class.getName());
 
     public static void main(String[] args) throws Exception {
-        Servers.start(new EchoServiceImpl(), logger);
+        Loggers.init();
+
+        int port = 50051;
+        Server server = ServerBuilder.forPort(port)
+            .addService(new EchoServiceImpl())
+            .build()
+            .start();
+
+        logger.info("server started, listening on "+ port);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.println("server is shutting down");
+            try {
+                server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                System.err.println("server shutdown was interrupted");
+                server.shutdownNow();
+            }
+            System.err.println("server has been shut down");
+        }));
+
+        server.awaitTermination();
     }
 
     static class EchoServiceImpl extends EchoServiceGrpc.EchoServiceImplBase {

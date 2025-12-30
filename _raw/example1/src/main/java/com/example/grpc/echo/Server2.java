db47b5d6f7b18b80bdf3eab8da0c895cd0e52c02
John Doe <john_doe@email.com>
2025-12-30 15:15:02 +0300

    Fix

---
+++ _raw/example1/src/main/java/com/example/grpc/echo/Server2.java
@@ -0,0 +1,38 @@
+package com.example.grpc.echo;
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class Server2 {
+
+    public static void start(BindableService bindableService, Logger logger) throws IOException, InterruptedException {
+        Logging.init();
+
+        int port = 50051;
+        Server server = ServerBuilder.forPort(port)
+            .addService(bindableService)
+            .build()
+            .start();
+
+        logger.log(Level.INFO, "server started, listening on {0}", port);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.println("server is shutting down");
+            try {
+                server.shutdown().awaitTermination(10, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                System.err.println("server shutdown was interrupted");
+                server.shutdownNow();
+            }
+            System.err.println("server has been shut down");
+        }));
+
+        server.awaitTermination();
+    }
+}

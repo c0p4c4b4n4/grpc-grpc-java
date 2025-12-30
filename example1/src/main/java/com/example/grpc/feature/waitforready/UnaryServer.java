6c17b696a9d959d01540a43d1975d675e2f298cf
John Doe <john_doe@email.com>
2025-12-30 16:16:39 +0300

    Fix

---
+++ example1/src/main/java/com/example/grpc/feature/waitforready/UnaryServer.java
@@ -0,0 +1,28 @@
+package com.example.grpc.feature.waitforready;
+
+import com.example.grpc.echo.EchoRequest;
+import com.example.grpc.echo.EchoResponse;
+import com.example.grpc.echo.EchoServiceGrpc;
+import com.example.grpc.echo.Servers;
+import io.grpc.stub.StreamObserver;
+
+import java.util.logging.Logger;
+
+public class UnaryServer {
+
+    private static final Logger logger = Logger.getLogger(UnaryServer.class.getName());
+
+    public static void main(String[] args) throws Exception {
+        Servers.start(new EchoServiceImpl(), logger);
+    }
+
+    static class EchoServiceImpl extends EchoServiceGrpc.EchoServiceImplBase {
+        @Override
+        public void unaryEcho(EchoRequest request, StreamObserver<EchoResponse> responseObserver) {
+            logger.info("request: "+ request.getMessage());
+            var response = EchoResponse.newBuilder().setMessage("hello " + request.getMessage()).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+    }
+}

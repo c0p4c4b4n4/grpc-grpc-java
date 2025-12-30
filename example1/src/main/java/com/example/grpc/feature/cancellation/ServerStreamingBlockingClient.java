97620d8cab402801f3e91f0bfa1bafff1188793a
John Doe <john_doe@email.com>
2025-12-30 16:31:16 +0300

    Fix

---
+++ example1/src/main/java/com/example/grpc/feature/cancellation/ServerStreamingBlockingClient.java
@@ -0,0 +1,35 @@
+package com.example.grpc.feature.cancellation;
+
+import com.example.grpc.echo.EchoRequest;
+import com.example.grpc.echo.EchoServiceGrpc;
+import com.example.grpc.echo.Loggers;
+import io.grpc.Deadline;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class ServerStreamingBlockingClient {
+
+    private static final Logger logger = Logger.getLogger(ServerStreamingBlockingClient.class.getName());
+
+    public static void main(String[] args) throws InterruptedException {
+        Loggers.init();
+
+        var channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+        try {
+            var blockingStub = EchoServiceGrpc.newBlockingStub(channel);
+            var request = EchoRequest.newBuilder().setMessage("world").build();
+            var responses = blockingStub.serverStreamingEcho(request);
+
+            while (responses.hasNext()) {
+                logger.info("response: " + responses.next().getMessage());
+            }
+        } catch (StatusRuntimeException e) {
+            logger.warning("error: " + e.getStatus());
+        } finally {
+            channel.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+}
---
+++ example1/src/main/java/com/example/grpc/feature/cancellation/ServerStreamingServer.java
@@ -0,0 +1,37 @@
+package com.example.grpc.feature.cancellation;
+
+import com.example.grpc.echo.EchoRequest;
+import com.example.grpc.echo.EchoResponse;
+import com.example.grpc.echo.EchoServiceGrpc;
+import com.example.grpc.echo.Servers;
+import io.grpc.stub.StreamObserver;
+
+import java.util.logging.Logger;
+
+public class ServerStreamingServer {
+
+    private static final Logger logger = Logger.getLogger(ServerStreamingServer.class.getName());
+
+    public static void main(String[] args) throws Exception {
+        Servers.start(new EchoServiceImpl(), logger);
+    }
+
+    static class EchoServiceImpl extends EchoServiceGrpc.EchoServiceImplBase {
+        @Override
+        public void serverStreamingEcho(EchoRequest request, StreamObserver<EchoResponse> responseObserver) {
+            logger.info("request: " + request.getMessage());
+            for (int i = 1; i <= 7; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                var message = "hello " + request.getMessage() + " " + i;
+                var response = EchoResponse.newBuilder().setMessage(message).build();
+                responseObserver.onNext(response);
+            }
+            responseObserver.onCompleted();
+        }
+    }
+}

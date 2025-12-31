64cfc4629f817b8c627fab15b7910b9da05d9c7c
John Doe <john_doe@email.com>
2025-12-31 19:09:42 +0300

    Fix

--- example1/src/main/java/com/example/grpc/features/healthservice/UnaryServer.java
+++ example1/src/main/java/com/example/grpc/features/healthservice/UnaryServer.java
@@ -6,7 +6,6 @@ import com.example.grpc.Loggers;
 import com.example.grpc.echo.EchoRequest;
 import com.example.grpc.echo.EchoResponse;
 import com.example.grpc.echo.EchoServiceGrpc;
-import com.example.grpc.features.keepalive.UnaryBlockingClient;
 import io.grpc.Grpc;
 import io.grpc.InsecureServerCredentials;
 import io.grpc.Server;
@@ -17,6 +16,7 @@ import io.grpc.stub.StreamObserver;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 
 public class UnaryServer extends Loggable {
@@ -30,12 +30,12 @@ public class UnaryServer extends Loggable {
         HealthStatusManager health = new HealthStatusManager();
 
         Server server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
-            .addService(new EchoServiceImpl())
+            .addService(new EchoServiceImpl(health))
             .addService(health.getHealthService())
             .build()
             .start();
 
-        logger.info("server started, listening on "+ port);
+        logger.info("server started, listening on " + port);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.err.println("server is shutting down");
@@ -55,16 +55,21 @@ public class UnaryServer extends Loggable {
 
     private static class EchoServiceImpl extends EchoServiceGrpc.EchoServiceImplBase {
 
-        boolean serving = true;
+        private final HealthStatusManager health;
+        private final AtomicBoolean serving = new AtomicBoolean(true);
+
+        public EchoServiceImpl(HealthStatusManager health) {
+            this.health = health;
+        }
 
         @Override
         public void unaryEcho(EchoRequest request, StreamObserver<EchoResponse> responseObserver) {
-            if (!serving) {
+            if (!serving.get()) {
                 responseObserver.onError(Status.INTERNAL.withDescription("not serving right now").asRuntimeException());
                 return;
             }
 
-            if (shouldHandle(request)) {
+            if (shouldServe(request)) {
                 EchoResponse response = EchoResponse.newBuilder().setMessage("hello " + request.getMessage()).build();
                 responseObserver.onNext(response);
                 responseObserver.onCompleted();
@@ -72,25 +77,25 @@ public class UnaryServer extends Loggable {
                 logger.warning("short message received, will not serve for 10 seconds");
                 health.setStatus("", ServingStatus.NOT_SERVING);
 
-                serving = false;
+                serving.set(false);
 
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
                         Delays.sleep(10);
-                        serving = true;
+                        serving.set(true);
 
-                        health.setStatus("", ServingStatus.SERVING);
                         logger.info("continue to serve");
+                        health.setStatus("", ServingStatus.SERVING);
                     }
                 }).start();
 
-                responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("offended by short message").asRuntimeException());
+                responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("paused by short message").asRuntimeException());
             }
         }
 
-        private boolean shouldHandle(EchoRequest request) {
-            return serving && request.getMessage().length() >= 5;
+        private boolean shouldServe(EchoRequest request) {
+            return serving.get() && request.getMessage().length() >= 5;
         }
     }
 }

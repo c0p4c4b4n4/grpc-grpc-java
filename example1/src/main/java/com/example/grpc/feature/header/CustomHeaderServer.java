a6ae0e0f85a2420c238656d4cf10c6a0cb6b894d
John Doe <john_doe@email.com>
2025-12-31 17:20:52 +0300

    Fix

--- example1/src/main/java/com/example/grpc/feature/header/CustomHeaderServer.java
+++ example1/src/main/java/com/example/grpc/feature/header/CustomHeaderServer.java
@@ -1,6 +1,5 @@
 package com.example.grpc.feature.header;
 
-
 import com.example.grpc.echo.EchoRequest;
 import com.example.grpc.echo.EchoResponse;
 import com.example.grpc.echo.EchoServiceGrpc;
@@ -9,65 +8,61 @@ import io.grpc.InsecureServerCredentials;
 import io.grpc.Server;
 import io.grpc.ServerInterceptors;
 import io.grpc.stub.StreamObserver;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-/**
- * A simple server that like {@link io.grpc.examples.helloworld.HelloWorldServer}.
- * You can get and response any header in {@link io.grpc.examples.header.HeaderServerInterceptor}.
- */
 public class CustomHeaderServer {
-  private static final Logger logger = Logger.getLogger(CustomHeaderServer.class.getName());
+    private static final Logger logger = Logger.getLogger(CustomHeaderServer.class.getName());
 
-  /* The port on which the server should run */
-  private static final int PORT = 50051;
-  private Server server;
+    private static final int PORT = 50051;
+    private Server server;
 
-  private void start() throws IOException {
-    server = Grpc.newServerBuilderForPort(PORT, InsecureServerCredentials.create())
-        .addService(ServerInterceptors.intercept(new EchoServiceImpl(), new HeaderServerInterceptor()))
-        .build()
-        .start();
-    logger.info("Server started, listening on " + PORT);
-    Runtime.getRuntime().addShutdownHook(new Thread() {
-      @Override
-      public void run() {
-        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-        System.err.println("*** shutting down gRPC server since JVM is shutting down");
-        try {
-          CustomHeaderServer.this.stop();
-        } catch (InterruptedException e) {
-          e.printStackTrace(System.err);
+    private void start() throws IOException {
+        server = Grpc.newServerBuilderForPort(PORT, InsecureServerCredentials.create())
+            .addService(ServerInterceptors.intercept(new EchoServiceImpl(), new HeaderServerInterceptor()))
+            .build()
+            .start();
+        logger.info("Server started, listening on " + PORT);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                try {
+                    CustomHeaderServer.this.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
         }
-        System.err.println("*** server shut down");
-      }
-    });
-  }
-
-  private void stop() throws InterruptedException {
-    if (server != null) {
-      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
     }
-  }
 
-  /**
-   * Await termination on the main thread since the grpc library uses daemon threads.
-   */
-  private void blockUntilShutdown() throws InterruptedException {
-    if (server != null) {
-      server.awaitTermination();
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
     }
-  }
 
-  /**
-   * Main launches the server from the command line.
-   */
-  public static void main(String[] args) throws IOException, InterruptedException {
-    final CustomHeaderServer server = new CustomHeaderServer();
-    server.start();
-    server.blockUntilShutdown();
-  }
+    /**
+     * Main launches the server from the command line.
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final CustomHeaderServer server = new CustomHeaderServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
 
     static class EchoServiceImpl extends EchoServiceGrpc.EchoServiceImplBase {
         @Override

fc5d6143862c9b3d0b3db78afa4aa1b888b486cd
John Doe <john_doe@email.com>
2026-01-02 17:54:02 +0300

    Fix

--- example1/src/main/java/com/example/grpc/healthservice/UnaryBlockingClient.java
+++ example1/src/main/java/com/example/grpc/healthservice/UnaryBlockingClient.java
@@ -1,13 +1,11 @@
 package com.example.grpc.healthservice;
 
 import com.example.grpc.Delays;
-import com.example.grpc.Loggable;
 import com.example.grpc.Loggers;
 import com.example.grpc.echo.EchoRequest;
 import com.example.grpc.echo.EchoServiceGrpc;
 import io.grpc.Grpc;
 import io.grpc.InsecureChannelCredentials;
-import io.grpc.ManagedChannel;
 import io.grpc.StatusRuntimeException;
 import io.grpc.health.v1.HealthCheckRequest;
 import io.grpc.health.v1.HealthGrpc;
@@ -64,7 +62,6 @@ public class UnaryBlockingClient {
             logger.info("greeting: " + response.getMessage());
         } catch (StatusRuntimeException e) {
             logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-            return;
         }
     }
 }

eb08d95702845d2d899a6d4a50434684676af5c0
John Doe <john_doe@email.com>
2026-01-05 17:29:31 +0300

    Fix

---
+++ _kotlin/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("com.google.protobuf") version protobufPluginVersion
+    id("com.gradleup.shadow") version shadowPluginVersion
+    application
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    // --- gRPC core ---
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("io.grpc:grpc-services:$grpcVersion")
+
+    // --- gRPC Kotlin ---
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+
+    // --- Protobuf Kotlin ---
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+
+    // --- Transport ---
+    runtimeOnly("io.grpc:grpc-netty-shaded:$grpcVersion")
+
+    // --- Kotlin ---
+    implementation(kotlin("stdlib"))
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+
+    plugins {
+        // Java gRPC codegen (optional but often useful)
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+
+        // Kotlin gRPC codegen
+        id("grpckt") {
+            artifact =
+                "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+        }
+    }
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    mergeServiceFiles()
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}

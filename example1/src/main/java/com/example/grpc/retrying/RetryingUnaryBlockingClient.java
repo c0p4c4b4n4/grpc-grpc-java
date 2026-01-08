447aca51d4a96f64fe0b001e182a0d1f41b7ede3
John Doe <john_doe@email.com>
2026-01-08 21:56:45 +0300

    Fix

--- example1/src/main/java/com/example/grpc/retrying/RetryingUnaryBlockingClient.java
+++ example1/src/main/java/com/example/grpc/retrying/RetryingUnaryBlockingClient.java
@@ -4,6 +4,7 @@ import com.example.grpc.Constants;
 import com.example.grpc.EchoRequest;
 import com.example.grpc.EchoServiceGrpc;
 import com.example.grpc.Loggers;
+import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
 
@@ -23,14 +24,7 @@ public class RetryingUnaryBlockingClient {
         Loggers.init();
 
         var enableRetries = !Boolean.parseBoolean(System.getenv("EXAMPLE_GRPC_DISABLE_RETRYING"));
-
-        var channelBuilder = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext();
-        if (enableRetries) {
-            var serviceConfig = getRetryingServiceConfig();
-            logger.info("client started with service configuration: " + serviceConfig);
-            channelBuilder.defaultServiceConfig(serviceConfig).enableRetry();
-        }
-        var channel = channelBuilder.build();
+        var channel = buildChannel(enableRetries);
         var blockingStub = EchoServiceGrpc.newBlockingStub(channel);
 
         final AtomicInteger totalCalls = new AtomicInteger();
@@ -58,6 +52,17 @@ public class RetryingUnaryBlockingClient {
             new Object[]{enableRetries ? "enabled" : "disabled", totalCalls.get(), failedCalls.get()});
 
         channel.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    }
+
+    private static ManagedChannel buildChannel(boolean enableRetries) {
+        var channelBuilder = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext();
+        if (enableRetries) {
+            var serviceConfig = getRetryingServiceConfig();
+            logger.info("client started with service configuration: " + serviceConfig);
+            channelBuilder.defaultServiceConfig(serviceConfig).enableRetry();
+        }
+        var channel = channelBuilder.build();
+        return channel;
     }
 
     private static Map<String, ?> getRetryingServiceConfig() {

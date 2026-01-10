ae97585f5e013d0d3fb1898cfd52b319d8ccd973
John Doe <john_doe@email.com>
2026-01-10 21:26:44 +0300

    Fix

--- grpc-kotlin-examples/src/main/kotlin/com/example/grpc/methodtypes/unary/UnaryBlockingClient.kt
+++ grpc-kotlin-examples/src/main/kotlin/com/example/grpc/methodtypes/unary/UnaryBlockingClient.kt
@@ -5,17 +5,22 @@ import com.example.grpc.echoRequest
 import io.grpc.ManagedChannelBuilder
 import io.grpc.StatusRuntimeException
 import java.util.concurrent.TimeUnit
+import java.util.logging.Logger
 
-suspend fun main(args: Array<String>) {
-  val channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build()
-  try {
-    val stub = EchoServiceGrpcKt.EchoServiceCoroutineStub(channel)
-    val request = echoRequest { this.message = "world" }
-    val response = stub.unaryEcho(request)
-    println("response: ${response.message}")
-  } catch (e: StatusRuntimeException) {
-    println("RPC error: ${e.status}")
-  } finally {
-    channel.shutdown().awaitTermination(10, TimeUnit.SECONDS)
+class UnaryBlockingClient {
+  private val logger = Logger.getLogger(UnaryBlockingClient::class.java.name)
+
+  suspend fun main(args: Array<String>) {
+    val channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build()
+    try {
+      val stub = EchoServiceGrpcKt.EchoServiceCoroutineStub(channel)
+      val request = echoRequest { this.message = "world" }
+      val response = stub.unaryEcho(request)
+      logger.info("response: ${response.message}")
+    } catch (e: StatusRuntimeException) {
+      logger.warning("RPC error: ${e.status}")
+    } finally {
+      channel.shutdown().awaitTermination(10, TimeUnit.SECONDS)
+    }
   }
 }
--- grpc-kotlin-examples/src/main/kotlin/com/example/grpc/methodtypes/unary/UnaryServer.kt
+++ grpc-kotlin-examples/src/main/kotlin/com/example/grpc/methodtypes/unary/UnaryServer.kt
@@ -6,34 +6,39 @@ import com.example.grpc.echoResponse
 import io.grpc.Server
 import io.grpc.ServerBuilder
 import java.util.concurrent.TimeUnit
+import java.util.logging.Logger
 
-fun main() {
-  val port = 50051
-  val server: Server = ServerBuilder
-    .forPort(port)
-    .addService(EchoServiceImpl())
-    .build()
-    .start()
+class UnaryServer {
+  private val logger = Logger.getLogger(UnaryServer::class.java.name)
 
-  println("server started, listening on $port")
+  fun main() {
+    val port = 50051
+    val server: Server = ServerBuilder
+      .forPort(port)
+      .addService(EchoServiceImpl())
+      .build()
+      .start()
 
-  Runtime.getRuntime().addShutdownHook(
-    Thread {
-      println("server is shutting down")
-      try {
-        server.shutdown().awaitTermination(10, TimeUnit.SECONDS)
-      } catch (e: InterruptedException) {
-        server.shutdownNow()
+    logger.info("server started, listening on $port")
+
+    Runtime.getRuntime().addShutdownHook(
+      Thread {
+        System.err.println("server is shutting down")
+        try {
+          server.shutdown().awaitTermination(10, TimeUnit.SECONDS)
+        } catch (e: InterruptedException) {
+          server.shutdownNow()
+        }
+        System.err.println("server has been shut down")
       }
-      println("server has been shut down")
-    },
-  )
+    )
 
-  server.awaitTermination()
-}
+    server.awaitTermination()
+  }
 
-private class EchoServiceImpl : EchoServiceGrpcKt.EchoServiceCoroutineImplBase() {
-  override suspend fun unaryEcho(request: EchoRequest) = echoResponse {
-    message = "hello ${request.message}"
+  private class EchoServiceImpl : EchoServiceGrpcKt.EchoServiceCoroutineImplBase() {
+    override suspend fun unaryEcho(request: EchoRequest) = echoResponse {
+      message = "hello ${request.message}"
+    }
   }
 }

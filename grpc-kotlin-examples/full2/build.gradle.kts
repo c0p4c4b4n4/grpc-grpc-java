cf216635defbd4478c7ff723403828ec5ae92fbb
John Doe <john_doe@email.com>
2026-01-10 18:09:50 +0300

    Kt

--- grpc-kotlin-examples/full2/build.gradle.kts
+++ grpc-kotlin-examples/full2/build.gradle.kts
@@ -1,35 +1,57 @@
 plugins {
-  application
-  alias(libs.plugins.kotlin.jvm)
+    application
+    kotlin("jvm") version "1.9.23"
+    id("com.google.protobuf") version "0.9.4"
 }
 
-kotlin { jvmToolchain(17) }
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
 
 dependencies {
-  implementation(project(":stub"))
-  runtimeOnly(libs.grpc.netty)
+    // gRPC + Protobuf
+    implementation("io.grpc:grpc-stub:1.63.0")
+    implementation("io.grpc:grpc-protobuf:1.63.0")
+    implementation("io.grpc:grpc-kotlin-stub:1.4.1")
+
+    implementation("com.google.protobuf:protobuf-kotlin:3.25.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+
+    // Transport (runtime only)
+    runtimeOnly("io.grpc:grpc-netty:1.63.0")
 }
 
-//tasks.register<JavaExec>("HelloWorldServer") {
-//  dependsOn("classes")
-//  classpath = sourceSets["main"].runtimeClasspath
-//  mainClass.set("io.grpc.examples.helloworld.HelloWorldServerKt")
-//}
-//
-//tasks.register<JavaExec>("RouteGuideServer") {
-//  dependsOn("classes")
-//  classpath = sourceSets["main"].runtimeClasspath
-//  mainClass.set("io.grpc.examples.routeguide.RouteGuideServerKt")
-//}
-//
-//tasks.register<JavaExec>("HelloWorldClient") {
-//    dependsOn("classes")
-//    classpath = sourceSets["main"].runtimeClasspath
-//    mainClass.set("io.grpc.examples.helloworld.HelloWorldClientKt")
-//}
-//
-//tasks.register<JavaExec>("RouteGuideClient") {
-//    dependsOn("classes")
-//    classpath = sourceSets["main"].runtimeClasspath
-//    mainClass.set("io.grpc.examples.routeguide.RouteGuideClientKt")
-//}
+application {
+    mainClass.set("com.example.main.AppKt")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.3"
+    }
+
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.63.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            task.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}

cfe9e49005ce9ea4161f1e123f2140f07d19206b
John Doe <john_doe@email.com>
2026-01-25 15:25:20 +0300

    Fix

---
+++ grpc-kotlin-examples/src/main/kotlin/com/example/grpc/deadline/DeadlineServerStreamingServer.kt
@@ -0,0 +1,32 @@
+package com.example.grpc.deadline
+
+import com.example.grpc.EchoRequest
+import com.example.grpc.EchoResponse
+import com.example.grpc.EchoServiceGrpcKt
+import com.example.grpc.Servers
+import com.example.grpc.echoResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.util.logging.Logger
+
+object DeadlineServerStreamingServer {
+  private val logger = Logger.getLogger(DeadlineServerStreamingServer::class.java.name)
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    Servers.start(EchoServiceImpl())
+  }
+
+  private class EchoServiceImpl : EchoServiceGrpcKt.EchoServiceCoroutineImplBase() {
+    override fun serverStreamingEcho(request: EchoRequest): Flow<EchoResponse> {
+      val name = request.message
+      logger.info("request: $name")
+
+      return flow {
+        emit(echoResponse { message = "hello $name" })
+        emit(echoResponse { message = "guten tag $name" })
+        emit(echoResponse { message = "bonjour $name" })
+      }
+    }
+  }
+}

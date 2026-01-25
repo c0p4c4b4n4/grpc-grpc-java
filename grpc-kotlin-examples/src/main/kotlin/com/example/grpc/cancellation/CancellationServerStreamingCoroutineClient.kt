e26c9d9c4386b0d09ad8336a976d784b7f4fce17
John Doe <john_doe@email.com>
2026-01-25 14:46:21 +0300

    Fix

--- grpc-kotlin-examples/src/main/kotlin/com/example/grpc/cancellation/CancellationServerStreamingCoroutineClient.kt
+++ grpc-kotlin-examples/src/main/kotlin/com/example/grpc/cancellation/CancellationServerStreamingCoroutineClient.kt
@@ -7,6 +7,9 @@ import io.grpc.ManagedChannelBuilder
 import io.grpc.StatusRuntimeException
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.TimeUnit
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.flow.take
 import java.util.logging.Logger
 
 object CancellationServerStreamingCoroutineClient {
@@ -19,13 +22,22 @@ object CancellationServerStreamingCoroutineClient {
     val channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build()
     try {
       val stub = EchoServiceGrpcKt.EchoServiceCoroutineStub(channel)
+      val request = echoRequest { message = "world" }
 
-      val request = echoRequest { this.message = "world" }
-      stub.serverStreamingEcho(request).collect { response ->
-        logger.info("response: ${response.message}")
-      }
+      logger.info("Starting stream...")
+
+      // .take(3) will automatically cancel the flow (and the RPC)
+      // after the 3rd element is received.
+      stub.serverStreamingEcho(request)
+        .take(3)
+        .collect { response ->
+          logger.info("Response received: ${response.message}")
+        }
+
     } catch (e: StatusRuntimeException) {
-      logger.warning("RPC error: ${e.status}")
+      logger.warning("RPC failed with status: ${e.status}")
+    } catch (e: CancellationException) {
+      logger.info("Client cancelled flow")
     } finally {
       channel.shutdown().awaitTermination(10, TimeUnit.SECONDS)
     }

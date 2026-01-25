a98689d473b47a053694023b55d4db667ac0fa44
John Doe <john_doe@email.com>
2026-01-25 17:37:51 +0300

    Fix

--- grpc-kotlin-examples/build.gradle.kts
+++ grpc-kotlin-examples/build.gradle.kts
@@ -1,48 +1,58 @@
 plugins {
-    application
+  application
 
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.protobuf)
+  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.protobuf)
 }
 
 dependencies {
-    api(libs.kotlinx.coroutines.core)
-    //implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${project.findProperty("grpcVersion")}")
+  api(libs.kotlinx.coroutines.core)
+  //implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${project.findProperty("grpcVersion")}")
 
-    api(libs.grpc.stub)
-    //implementation("io.grpc:grpc-stub:${project.findProperty("grpcVersion")}")
-    api(libs.grpc.protobuf)
-    //implementation("io.grpc:grpc-protobuf:${project.findProperty("grpcVersion")}")
-    api(libs.protobuf.java.util)
-    //implementation("com.google.protobuf:protobuf-java-util:${project.findProperty("grpcVersion")}")
-    api(libs.protobuf.kotlin)
-    //implementation("com.google.protobuf:protobuf-kotlin:${project.findProperty("grpcVersion")}")
-    api(libs.grpc.kotlin.stub)
-    //implementation("io.grpc:grpc-kotlin-stub:${project.findProperty("grpcVersion")}")
+  api(libs.grpc.stub)
+  //implementation("io.grpc:grpc-stub:${project.findProperty("grpcVersion")}")
+  api(libs.grpc.protobuf)
+  //implementation("io.grpc:grpc-protobuf:${project.findProperty("grpcVersion")}")
+  api(libs.protobuf.java.util)
+  //implementation("com.google.protobuf:protobuf-java-util:${project.findProperty("grpcVersion")}")
+  api(libs.protobuf.kotlin)
+  //implementation("com.google.protobuf:protobuf-kotlin:${project.findProperty("grpcVersion")}")
+  api(libs.grpc.kotlin.stub)
+  //implementation("io.grpc:grpc-kotlin-stub:${project.findProperty("grpcVersion")}")
 
 //    runtimeOnly(libs.grpc.netty)
-    runtimeOnly("io.grpc:grpc-netty-shaded:${project.findProperty("grpcVersion")}")
+  runtimeOnly("io.grpc:grpc-netty-shaded:${project.findProperty("grpcVersion")}")
 }
 
-kotlin { jvmToolchain(17) }
+kotlin {
+  jvmToolchain(17)
+}
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    compilerOptions { freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn") }
+  compilerOptions { freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn") }
 }
 
 protobuf {
-    protoc { artifact = libs.protoc.asProvider().get().toString() }
-    plugins {
-        create("grpc") { artifact = libs.protoc.gen.grpc.java.get().toString() }
-        create("grpckt") { artifact = libs.protoc.gen.grpc.kotlin.get().toString() + ":jdk8@jar" }
+  protoc {
+    artifact = libs.protoc.asProvider().get().toString()
+  }
+  plugins {
+    create("grpc") {
+      artifact = libs.protoc.gen.grpc.java.get().toString()
     }
-    generateProtoTasks {
-        all().forEach {
-            it.plugins {
-                create("grpc")
-                create("grpckt")
-            }
-            it.builtins { create("kotlin") }
-        }
+    create("grpckt") {
+      artifact = libs.protoc.gen.grpc.kotlin.get().toString() + ":jdk8@jar"
     }
+  }
+  generateProtoTasks {
+    all().forEach {
+      it.plugins {
+        create("grpc")
+        create("grpckt")
+      }
+      it.builtins {
+        create("kotlin")
+      }
+    }
+  }
 }
--- grpc-kotlin-examples/settings.gradle.kts
+++ grpc-kotlin-examples/settings.gradle.kts
@@ -1,18 +1,1 @@
 rootProject.name = "grpc-kotlin-examples"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-    }
-}
-
-dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
-    repositories {
-        mavenCentral()
-        google()
-    }
-}
-
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
